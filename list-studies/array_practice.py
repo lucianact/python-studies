43e57775207a736927a60cb1c9bf40b44204e18e
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-03-05 18:52:18 +0000

    Add miscellaneous-tasks

--- list-studies/array_practice.py
+++ list-studies/array_practice.py
@@ -1,34 +1,7 @@
-# Your company built an in-house calendar tool called HiCal. 
-# You want to add a feature to see the times in a day when everyone is available.
-
-# To do this, you’ll need to know when any team is having a meeting. 
-# In HiCal, a meeting is stored as a tuple of integers:
-# (start_time, end_time)
-# These integers represent the number of 30-minute blocks past 9:00am.
-
-# For example:
-# (2, 3)  # Meeting from 10:00 – 10:30 am
-# (6, 9)  # Meeting from 12:00 – 1:30 pm
-
-# Write a function merge_ranges() 
+# 1
+# # Write a function merge_ranges() 
 # that takes a list of multiple meeting time ranges 
 # and returns a list of condensed ranges.
-
-# def merge_ranges(meetings):
-#     sorted_meetings = sorted(meetings)
-#     print(sorted_meetings)
-#     condensed_meetings = []
-#     start_index = 0
-#     while start_index < (len(sorted_meetings) - 1):
-#         if (sorted_meetings[start_index][1] == sorted_meetings[start_index+1][0]) or (sorted_meetings[start_index][1] > sorted_meetings[start_index+1][0]):
-#             new_tuple = (sorted_meetings[start_index][0],sorted_meetings[start_index+1][1])
-#             condensed_meetings.append(new_tuple)
-#             sorted_meetings.pop(start_index+1)
-#         else:
-#             condensed_meetings.append(sorted_meetings[start_index])
-#         start_index += 1
-#     return condensed_meetings
-
 def merge_ranges(meetings):
 
     # Sort by start time
@@ -56,15 +29,13 @@ def merge_ranges(meetings):
 
     return merged_meetings
 
+# meetings = [(1, 10), (2, 6), (3, 5), (7, 9)]
+# print(merge_ranges(meetings))
 
-meetings = [(1, 10), (2, 6), (3, 5), (7, 9)]
-print(merge_ranges(meetings))
-
+# 2
 # Write a function that takes a list of characters 
 # and reverses the letters in place:
-
-# Note: not askinf to reverse in sorted/lexicographic order! 
-
+# Note: not asking to reverse in sorted/lexicographic order! 
 def reverse_chars(list_of_chars):
 
     left_index = 0
@@ -81,13 +52,13 @@ def reverse_chars(list_of_chars):
     
     return(list_of_chars)
 
-list_of_chars = ["n", "i", "c", "o"]
-print(reverse_chars(list_of_chars))
+# list_of_chars = ["n", "i", "c", "o"]
+# print(reverse_chars(list_of_chars))
 
+# 3
 # Write a function reverse_words() that takes a message 
 # as a list of characters and reverses 
 # the order of the words in place
-
 def reverse_words(message):
    
     string_message = "".join(message).split(" ")
@@ -102,11 +73,11 @@ message = [ 'c', 'a', 'k', 'e', ' ',
             'p', 'o', 'u', 'n', 'd', ' ',
             's', 't', 'e', 'a', 'l' ]
 
-print(reverse_words(message))
+# print(reverse_words(message))
 
+# 4
 # We have our lists of orders sorted numerically already, in lists. 
 # Write a function to merge our lists of orders into one sorted list.
-
 def merge_lists(my_list, alices_list):
     # return sorted(arr1 + arr2)
 
@@ -132,4 +103,70 @@ def merge_lists(my_list, alices_list):
 my_list     = [3, 4, 6, 10, 11, 15]
 alices_list = [1, 5, 8, 12, 14, 19]
 
-print(merge_lists(my_list, alices_list))
+# print(merge_lists(my_list, alices_list))
+
+# 5
+# Write a function to check that my service is first-come, first-served. 
+# All food should come out in the same order customers requested it.
+# We'll represent each customer order as a unique integer.
+def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
+    
+    take_out_orders_index = 0
+    dine_in_orders_index = 0
+    take_out_orders_max_index = len(take_out_orders) - 1
+    dine_in_orders_max_index = len(dine_in_orders) - 1
+
+    for order in served_orders:
+        # If we still have orders in take_out_orders
+        # and the current order in take_out_orders is the same
+        # as the current order in served_orders
+        if (take_out_orders_index <= take_out_orders_max_index and 
+                order == take_out_orders[take_out_orders_index]):
+            take_out_orders_index += 1
+
+        # If we still have orders in dine_in_orders
+        # and the current order in dine_in_orders is the same
+        # as the current order in served_orders
+        elif (dine_in_orders_index <= dine_in_orders_max_index 
+                and order == dine_in_orders[dine_in_orders_index]):
+            dine_in_orders_index += 1
+
+        # If the current order in served_orders doesn't match the current
+        # order in take_out_orders or dine_in_orders, then we're not serving first-come,
+        # first-served
+        else:
+            return False
+
+    # Check for any extra orders at the end of take_out_orders or dine_in_orders
+    if (dine_in_orders_index != len(dine_in_orders) or
+        take_out_orders_index != len(take_out_orders)):
+        return False
+
+
+    if served_orders == []:
+        return False
+
+    elif take_out_orders == [] or dine_in_orders == []:
+        if take_out_orders != served_orders or dine_in_orders != served orders:
+            return False
+
+    elif take_out_orders[0] == served_orders[0] or dine_in_orders[0] == served_orders[0]:
+
+    take_out_orders_index = 0
+    dine_in_orders_index = 0
+    served_orders_index = 0
+
+        while take_out_orders_index or dine_in_orders_index <= len(served_orders) - 1:
+
+            if take_out_orders[take_out_orders_index] == served_orders[served_orders_index]:
+            take_out_orders_index += 1
+            served_orders_index += 1
+
+            elif dine_in_orders[dine_in_orders_index] == served_orders[served_orders_index]:
+            dine_in_orders_index += 1
+            take_out_orders_index +1
+        
+            else:
+                return False
+        
+        return True 
