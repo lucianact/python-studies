c8914a066c3740faf2e25a8dc19844bac98bf35d
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-03-08 21:38:03 +0000

    Implement bubble sort

---
+++ list-studies/sorting.py
@@ -0,0 +1,28 @@
+# Bubble Sort:
+# takes an unsorted list and orders it in ascending values
+# lowest number will be in the beginning of the list
+# highest number will be in the end
+# we do that by comparing two side by side values
+
+def bubble_sort(lst):
+    """Bubble the highest number to the end"""
+
+    # nested for loop reminder:
+    # the "inner loop" will be executed one time
+    # for each iteration of the "outer loop"
+    for i in range(len(lst) - 1):
+        made_swap = False
+        # if we want to make it shorter, replace 
+        # [for j in range(len(lst) - 1)] for:
+        for j in range(len(lst) - 1 - i):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                made_swap = True
+        if not made_swap:
+            break
+
+    return lst
+
+lst = [6, 4, 8, 3, 2, 5, 7, 8, 9]
+print(bubble_sort(lst))
+
