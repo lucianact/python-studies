a534edf5e81d01d1b594f52299a1437a4621c6e4
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-03-15 18:55:36 +0000

    Implement singly LL with no tail

---
+++ linked-lists/linkedlists.py
@@ -0,0 +1,67 @@
+class Node(object):
+    """Node in a Linked List."""
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return f"Node data: {self.data}, next node: {self.next.data}" if self.next else None
+
+class NoTailLinkedList(object):
+    """Linked List using head only."""
+    
+    def __init__(self):
+        self.head = None
+    
+    def append_node(self, data):
+        """Append node with data to end of list."""
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+        # the last node of the ll will always point to none
+        else:
+            current_node = self.head
+            while current_node.next is not None:
+                current_node = current_node.next
+            current_node.next = new_node
+    
+    def print_list(self):
+        """Print all items in the list."""
+
+        current_node = self.head
+
+        while current_node is not None:
+            print(current_node.data)
+            current_node = current_node.next 
+
+
+# class LinkedList(object):
+#     """Linked List using head and tail."""
+    
+#     def __init__(self):
+#         self.head = None
+#         self.tail = None
+        
+#     def append(self, data):
+#         """Append node with data to the end of the list."""
+
+#         # creating a node inside the method 
+#         new_node = Node(data)
+
+#         # should the new node be the head of our ll?
+#         if self.head is None:
+#             self.head = new_node
+        
+#         # if the ll is not empty:
+#         if self.tail is not None:
+#             self.tail.next = new_node
+        
+#         # while the ll is empty:
+#         self.tail = new_node
+
+# ll = LinkedList()
+# ll.append(1)
+
+
