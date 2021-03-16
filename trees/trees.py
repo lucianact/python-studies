5e725ebe54c45ed923a4bd17dfc966f8e4210482
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-03-16 00:33:23 +0000

    Add terminology

---
+++ trees/trees.py
@@ -0,0 +1,9 @@
+# terminology:
+# node -> basic unit
+# children -> nodes directly below a node
+# descendants -> nodes below a node
+# parent -> node that is directly abovea node
+# ancestor -> node that is above a node
+# root nod -> node at the top of the three
+# left node -> node without any children 
+
