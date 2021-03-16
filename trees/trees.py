13c32abd44211ee64e49ed7bab04e33b9ab64abc
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-03-16 00:38:41 +0000

    Add node class

--- trees/trees.py
+++ trees/trees.py
@@ -1,4 +1,4 @@
-# terminology:
+# Terminology:
 # node -> basic unit
 # children -> nodes directly below a node
 # descendants -> nodes below a node
@@ -6,4 +6,20 @@
 # ancestor -> node that is above a node
 # root nod -> node at the top of the three
 # left node -> node without any children 
+# Examples:
+# a filesystem is a tree
+# html dom is a tree
+# taxonomy is a tree
 
+class Node(object):
+    """Node in a tree."""
+    
+    def __init__(self, data, children):
+        self.data = data
+        self.children = children 
+
+    def __repr__(self):
+        """Reader-friendly representation."""
+        
+        return f"Node is {self.data}"
+    
