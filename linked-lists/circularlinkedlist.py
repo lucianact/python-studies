0e351407e39e36a0c34eeec66effe6f87cce01d3
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-04-13 21:40:45 +0000

    Update append and prepend  method

--- linked-lists/circularlinkedlist.py
+++ linked-lists/circularlinkedlist.py
@@ -10,8 +10,8 @@ class CircularLinkedList(object):
     def __init__(self, head=None):
         self.head = None
 
-    def prepend_node(self, new_node):
-        """Prepend a new node to a circular linked list.""" 
+    def append_node(self, new_node):
+        """Append a new node to a circular linked list.""" 
 
         # create a new node:
         new_node = Node(new_node)
@@ -25,10 +25,10 @@ class CircularLinkedList(object):
         # the last node instead of poiting to None, 
         # now poitns back to the head node. 
 
-        # so when we add a new head to our linked list, 
-        # besides updating the head node and the current 
-        # .next head node pointer, we also need to 
-        # update the last node .next pointer:
+        # so when we append a new node to our linked list, 
+        # we also need to update the last node .next pointer
+        # to point to the new node and the new node .next
+        # pointer should now point to the node head
         current_node = self.head
         while current_node.next != self.head:
             current_node = current_node.next
@@ -36,8 +36,8 @@ class CircularLinkedList(object):
         new_node.next = self.head
     
 
-    def append_node(self, new_node):
-        """Append a new node to a circular linked list."""
+    def prepend_node(self, new_node):
+        """Prepend a new node to a circular linked list."""
         
         # create a new node:
         new_node = Node(new_node)
@@ -57,6 +57,7 @@ class CircularLinkedList(object):
             current_node = current_node.next
         current_node.next = new_node
         new_node.next = self.head 
+        self.head = new_node
         
     
     def print_linked_list(self): 
