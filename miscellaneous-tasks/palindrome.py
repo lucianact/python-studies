fe49a95fe70bfd6ce27b243c1a78094dc942f592
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-04-21 19:55:06 +0000

    Add  palindrome

---
+++ miscellaneous-tasks/palindrome.py
@@ -0,0 +1,10 @@
+# def isPalindrome(s):
+#     return s == s[::-1] # how?
+
+def isPalindrome(str):
+ 
+    # Run loop from 0 to len/2
+    for i in range(0, int(len(str)/2)):
+        if str[i] != str[len(str)-i-1]:
+            return False
+    return True
