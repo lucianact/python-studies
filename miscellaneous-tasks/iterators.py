784926d388d502101d5d905b328c93208e83655b
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-04-23 20:34:44 +0000

    Add iter()

--- miscellaneous-tasks/iterators.py
+++ miscellaneous-tasks/iterators.py
@@ -1,6 +1,13 @@
 """A look into Python iterators""" 
 
+week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+semana = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"]
+
 # iter()
+i = iter(week)
+print(i)
+print(next(i))
+
 # .readline
 # range(len)
 # enumerate() 
