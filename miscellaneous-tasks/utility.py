4b014243abfadb650f9ea1419e06e7b28557b8a9
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-04-23 20:28:42 +0000

    Add any(), all(), min(), max(), sum()

---
+++ miscellaneous-tasks/utility.py
@@ -0,0 +1,17 @@
+"""Some useful built-in functions"""
+
+# any() - if any value is True
+values = [1, 2, 3, 4, 0]
+print(any(values))
+
+# all() -  if all values are True
+print(all(values))
+
+# min()
+print(min(values))
+
+# max()
+print(max(values))
+
+# sum()
+print(sum(values))
