82a5fe65eabdf95f9131f81f31bfe8200330bbae
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-04-24 01:22:58 +0000

    Add comments

--- miscellaneous-tasks/transformers.py
+++ miscellaneous-tasks/transformers.py
@@ -37,3 +37,36 @@ def letter_grade(w):
 grades = (90, 60, 70, 80)
 grades_converted = list(map(letter_grade, grades))
 print(grades_converted)
+
+
+# # Write a function that returns the sum of integers in an
+# # arbitrarily nested list of lists, where each integer is
+# # weighted multiplicatively by the "depth" of its level.
+# # For example, the following inputs should return the following outputs:
+# input_A = [1, 2, 3]
+# #weighted_sum(input_A) == 6
+# input_B = [1, [2, 3]]
+# #weighted_sum(input_B) == 1 * 1 + (2 + 3) * 2 ==  11
+# input_C = [1, [2, [3, 4]]]
+# #weighted_sum(input_C) == 1 * 1 + 2 * 2 + (3 + 4) * 3 == 26
+
+# function greet (person) {
+#   if (person == { name: 'amy' }) {
+#     return 'hey amy'
+#   } else {
+#     return 'hey arnold'
+#   }
+# }
+
+# console.log(greet({ name: 'amy' }))
+
+# let amy = { name: 'amy' }
+
+# function greet (person) {
+#   if (person.name == 'amy') {
+#     return 'hey amy'
+#   } else {
+#     return 'hey arnold'
+#   }
+# }
+# console.log(greet(amy))
