f502ef948fd7b579f53bce1e3664377110e05cf5
Luciana Terrazas <luciana.cterrazas@gmail.com>
2021-04-25 19:02:26 +0000

    Find the missing number

--- miscellaneous-tasks/random_exercises.py
+++ miscellaneous-tasks/random_exercises.py
@@ -72,3 +72,70 @@ print(new_dict)
 # check all the keys 
 # to be able to sort the key-value pair
 # This is not a good idea
+
+
+# 4
+# Given an array nums containing n distinct numbers in the range [0, n], 
+# return the only number in the range that is missing from the array.
+
+# Example 1:
+# Input: nums = [3,0,1]
+# Output: 2
+# Explanation: n = 3 since there are 3 numbers, 
+# so all numbers are in the range [0,3]. 2 is the missing number 
+# in the range since it does not appear in nums.
+# Example 2:
+# Input: nums = [0,1]
+# Output: 2
+# Explanation: n = 2 since there are 2 numbers, 
+# so all numbers are in the range [0,2]. 
+# 2 is the missing number in the range since 
+# it does not appear in nums.
+# Example 3:
+# Input: nums = [9,6,4,2,3,5,7,0,1]
+# Output: 8
+# Explanation: n = 9 since there are 9 numbers, 
+# so all numbers are in the range [0,9]. 
+# 8 is the missing number in the range since 
+# it does not appear in nums.
+# Example 4:
+# Input: nums = [0]
+# Output: 1
+# Explanation: n = 1 since there is 1 number, 
+# so all numbers are in the range [0,1].
+# 1 is the missing number in the range 
+# since it does not appear in nums.
+ 
+# Constraints:
+# n == nums.length
+# 1 <= n <= 10^4
+# 0 <= nums[i] <= n
+# All the numbers of nums are unique.
+
+def missingNumber(nums):
+    
+    n = len(nums)
+    print(n)
+    
+    sum1 = 0
+    for i in range(n+1):
+        sum1 += i 
+    print(sum1)
+    
+    sum2 = 0
+    for num in nums:
+        sum2 += num
+    print(sum2)
+    
+    value = sum1 - sum2
+    print(value)
+    
+    return value 
+    
+    # checking the len of array
+    # -> 9 
+    # sum nums range (0-9)
+    # 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = 45
+    # sum of n - sum of num = missing number 
+
+print(missingNumber([9,6,4,2,3,5,7,0,1])) 
